42488d64b86002e111d850f4acb56c8ba73e378a
unknown <sargey7@gmail.com>
2021-09-14 14:31:08 +0300

    first commit

---
+++ MVC_Store/Models/Data/Db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.Data
+{
+    public class Db :DbContext
+    {
+    }
+}
---
+++ MVC_Store/areas/admin/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.areas.admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: admin/Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
